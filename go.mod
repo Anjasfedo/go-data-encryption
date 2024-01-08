eaa185d330f9b43520d610d1671c6481e59cefd8
AnjasFedo <fedoaf98@gmail.com>
2024-01-08 09:41:00 +0700

    Encrypt &  Decrypt Function

--- go.mod
+++ go.mod
@@ -1,4 +1,3 @@
 module github.com/G1A021037-ANJASFEDO/go-data-encryption
 
 go 1.21.5
-
--- main.go
+++ main.go
@@ -1,17 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func hashLetter(key int, letter string) (result string) {
+	runes := []rune(letter)
+
+	lastLetters := string(runes[len(letter)-key : len(letter)])
+
+	restLetters := string(runes[0 : len(letter)-key])
+
+	return fmt.Sprintf(`%s%s`, lastLetters, restLetters)
+}
 
 func Encrypt(key int, plainText string) (result string) {
+	hashLetter := hashLetter(key, originalLetter)
 
+	var hashedString = ""
+
+	findOne := func(r rune) rune {
+		position := strings.Index(originalLetter, string([]rune{r}))
+
+		if position != -1 {
+			letterPosition := (position + len(originalLetter)) % len(originalLetter)
+
+			hashedString = hashedString + string(hashLetter[letterPosition])
+
+			return r
+		}
+
+		return r
+	}
+
+	strings.Map(findOne, plainText)
+
+	return hashedString
 }
 
 func Decrypt(key int, CipherText string) (result string) {
+	hashLetter := hashLetter(key, originalLetter)
 
+	var hashedString = ""
+
+	findOne := func(r rune) rune {
+		position := strings.Index(hashLetter, string([]rune{r}))
+
+		if position != -1 {
+			letterPosition := (position + len(originalLetter)) % len(originalLetter)
+
+			hashedString = hashedString + string(originalLetter[letterPosition])
+
+			return r
+		}
+
+		return r
+	}
+
+	strings.Map(findOne, CipherText)
+
+	return hashedString
 }
 
 func main() {
-	plainText := "ANJAS"
+	plainText := "ANJASFEDO"
 	fmt.Println("Plain Text: ", plainText)
 
 	encrypted := Encrypt(5, plainText)
